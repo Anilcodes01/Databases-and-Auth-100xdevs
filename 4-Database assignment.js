b7b693cbe49d047c122cf304a8d21b316a55f25c
Anilcodes01 <anilcodes01@gmail.com>
2024-03-21 15:36:15 +0530

    Connecting backend to the frontend

--- 4-Database assignment.js
+++ 4-Database assignment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require('cors')
 mongoose.connect(
     "mongodb+srv://anilcodes01:anilcodes01@cluster0.kbsq56y.mongodb.net/user_app_new"
   );
@@ -8,6 +8,15 @@ mongoose.connect(
 
 const app = express();
 app.use(express.json())
+app.use(cors())
+
+// Middleware to allow CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 const User = mongoose.model("Users", {
   name: String,
@@ -16,11 +25,11 @@ const User = mongoose.model("Users", {
 });
 
 app.post('/signup', async (req, res) => {
-    const username = req.body.username;
+    const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    const existingUser = await User.findOne({email: username})
+    const existingUser = await User.findOne({email: email})
     if(existingUser) {
         return res.status(400).send('User already exists!')
     }
